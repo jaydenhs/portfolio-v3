a957dcd7d870d9f36faad4fa2412db2c335eb925
jaydenhs <j3hsiao@uwaterloo.ca>
2023-08-12 12:34:39 -0400

    Fix duplicate text being shown with mode="wait"

--- src/components/home/text-reveal.jsx
+++ src/components/home/text-reveal.jsx
@@ -2,19 +2,17 @@ import React from "react"
 import { AnimatePresence } from "framer-motion"
 import { motion } from "framer-motion"
 
-// look into individual letter reveal https://codepen.io/bekahmcdonald/pen/vYBXMOZ
-
 export default function TextReveal({ currentWord }) {
   return (
     <>
       <div className="w-full relative h-16 overflow-hidden">
-        <AnimatePresence>
+        <AnimatePresence mode="wait">
           <motion.h1
             key={currentWord}
             initial={{ y: -60 }}
             animate={{ y: 0 }}
             exit={{ y: 60 }}
-            transition={{ duration: 0.4 }}
+            transition={{ duration: 0.2 }}
             className="absolute text-blue-500 whitespace-nowrap"
           >
             {currentWord}
