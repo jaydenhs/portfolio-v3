0cabb9ea4bfe8f35fa1589ec90104ad6aeab5b23
jaydenhs <j3hsiao@uwaterloo.ca>
2023-08-13 22:55:48 -0400

    fix Metrics component (bug in prod)

--- src/components/work/metrics.jsx
+++ src/components/work/metrics.jsx
@@ -4,15 +4,13 @@ import tw from "twin.macro"
 
 export default function Metrics({ disclaimer = false, children }) {
   return (
-    <blockquote className="flex flex-col items-center py-8">
-      <QuoteContent className="text-center space-y-3">
-        {children}
-        {disclaimer && (
-          <p className="text-gray-400 font-normal text-base pt-2">
-            (actual values omitted for confidentiality reasons)
-          </p>
-        )}
-      </QuoteContent>
+    <blockquote className="flex flex-col items-center py-8 space-y-5">
+      <QuoteContent className="text-center space-y-3">{children}</QuoteContent>
+      {disclaimer && (
+        <p className="text-gray-400 font-normal text-base">
+          (actual values omitted for confidentiality reasons)
+        </p>
+      )}
     </blockquote>
   )
 }
