20dc194544ce178f460a189b82bb11e7c6b9ca3b
jaydenhs <j3hsiao@uwaterloo.ca>
2023-08-14 11:51:03 -0400

    Add header and About page

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useRef } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import "../styles/global.css"
 
 import Header from "./header"
 
 export default function App({ children, className }) {
+  const scrollContainerRef = useRef(null)
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -16,20 +18,13 @@ export default function App({ children, className }) {
   `)
 
   return (
-    <div className="scroll-container">
+    <div className="scroll-container" ref={scrollContainerRef}>
+      <Header
+        scrollContainerRef={scrollContainerRef}
+        siteTitle={data.site.siteMetadata?.title || `Title`}
+      />
       <div className="scroll-content">
-        <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-        <main>{children}</main>
-        {/* <footer
-        style={{
-          marginTop: `var(--space-5)`,
-          fontSize: `var(--font-sm)`,
-        }}
-      >
-        © {new Date().getFullYear()} &middot; Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer> */}
+        <main className="pt-16">{children}</main>
       </div>
     </div>
   )
---
+++ src/pages/about.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import App from "../components/app"
+
+export default function AboutPage() {
+  return <App>About page here!</App>
+}
